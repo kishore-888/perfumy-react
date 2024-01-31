9a2270cefdd9465bf2a2fc2d086c6cddaf24f225
kishore-888 <kishorekcu57075@gmail.com>
2024-01-31 22:21:37 +0530

    Update project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar"
+import Search from "./components/Search"
+import Product from "./components/Product"
+import About from "./components/About"
+import Footer from "./components/Footer"
+
+function App() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Search></Search>
+            <Product></Product>
+            <About></About>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App
